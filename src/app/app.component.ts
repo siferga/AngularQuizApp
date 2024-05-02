2663594e9f051d64cd115ab54f891123c91d51e7
Siferga <sifergadeveloper@gmail.com>
2024-05-02 14:03:09 +0200

    app component update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { UserModel } from './models/user.model';
+import {UserService} from "./user.service";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'AngularQuizApp';
+export class AppComponent implements OnInit{
+  public users!: UserModel[];// Marqué comme undefined
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.getUsers()
+  }
+
+  public getUsers():void {
+    this.userService.getUsers().subscribe(
+      (response:UserModel[]) => {
+        this.users = response;
+      },
+      (error:HttpErrorResponse) => {
+        alert(error.message);
+      }
+    )
+ }
 }
