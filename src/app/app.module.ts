d7425bdbfca829303f9d6613bf31bd812c7ab253
Siferga <sifergadeveloper@gmail.com>
2024-05-02 10:14:35 +0200

    header and footer component creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component'; //Ajouter le support HTTP Client
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule //Ajouter le support HTTP Client
   ],
   providers: [],
   bootstrap: [AppComponent]
