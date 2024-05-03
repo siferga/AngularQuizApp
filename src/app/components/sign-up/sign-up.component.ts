ee7cf51c8fb5f520388e0ecfa6a1f02fe84b558e
Siferga <sifergadeveloper@gmail.com>
2024-05-03 09:23:29 +0200

    sign up component creation

---
+++ src/app/components/sign-up/sign-up.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrl: './sign-up.component.css'
+})
+export class SignUpComponent {
+  signUpForm = {
+    first_name: '',
+    last_name: '',
+    user_name: '',
+    email: '',
+    password: ''};
+
+  constructor(private authService: AuthService) {}
+
+  onSubmit(): void {
+    this.authService.signUp(this.signUpForm).subscribe(
+      response => {
+        console.log('Registration successful', response);
+      },
+      error => {
+        console.error('Registration failed', error);
+      }
+    );
+  }
+
+}
