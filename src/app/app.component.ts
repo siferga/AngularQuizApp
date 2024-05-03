2f4280d1335a74b5b52e0ef19977e355db369dce
Siferga <sifergadeveloper@gmail.com>
2024-05-03 09:24:38 +0200

    app components update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import { User } from './models/user';
 import {UserService} from "./services/user.service";
 import {HttpErrorResponse} from "@angular/common/http";
+import {NgForm} from "@angular/forms";
+
 
 @Component({
   selector: 'app-root',
@@ -10,21 +12,44 @@ import {HttpErrorResponse} from "@angular/common/http";
 })
 export class AppComponent implements OnInit{
   users!: User[];// Marqué comme undefined
+  editUser!: User;
+
+
+  emailError: string = '';
+  passwordError: string = '';
 
   constructor(private userService: UserService) {}
 
   ngOnInit() {
-    this.getUsers()
+    this.getUsers();
   }
 
   public getUsers():void {
     this.userService.getUsers().subscribe(
       (response:User[]) => {
         this.users = response;
+        console.log(this.users);
       },
       (error:HttpErrorResponse) => {
         alert(error.message);
       }
     )
  }
+
+  public onOpenModal(user: User, mode: string): void {
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if (mode === 'add') {
+      button.setAttribute('data-target', '#addUserModal');
+    }
+
+   /* container.appendChild(button);
+    button.click();
+    container.remove(button);*/
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component'; //Ajouter le support HTTP Client
+import { FooterComponent } from './components/footer/footer.component';
+import {FormsModule} from "@angular/forms";
+import {UserService} from "./services/user.service";
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component'; //Ajouter le support HTTP Client
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    SignInComponent,
+    SignUpComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule //Ajouter le support HTTP Client
-  ],
-  providers: [],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        FormsModule,
+        //Ajouter le support HTTP Client
+    ],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
