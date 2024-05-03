fda28c09ce4e6ebe8faee4692f4ae0453f7b7b48
Siferga <sifergadeveloper@gmail.com>
2024-05-03 09:59:09 +0200

    sing in and sign out forms integration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {SignInComponent} from "./components/sign-in/sign-in.component";
+import {SignUpComponent} from "./components/sign-up/sign-up.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'sign-up', component: SignUpComponent },
+  { path: '', redirectTo: '/sign-in', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/sign-in/sign-in.component.ts
+++ src/app/components/sign-in/sign-in.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {AuthService} from "../../services/auth.service";
 
 @Component({
   selector: 'app-sign-in',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrl: './sign-in.component.css'
 })
 export class SignInComponent {
+  signInForm = {
+    email: '',
+    password: ''};
 
+  constructor(private authService: AuthService) {
+
+  }
+
+  onSubmit(): void {
+    this.authService.signIn(this.signInForm.email, this.signInForm.password).subscribe(
+      response => {
+        console.log('Connexion successful', response);
+      },
+      error => {
+        console.error('Connexion failed', error);
+      }
+    );
+  }
 }
