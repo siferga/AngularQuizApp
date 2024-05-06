1a01ee849651ae1c4b37fff859652b1d043c2104
Siferga <sifergadeveloper@gmail.com>
2024-05-06 12:27:23 +0200

    updates

--- src/app/components/result/result.component.ts
+++ src/app/components/result/result.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
+import {HeaderComponent} from "../header/header.component";
+import {FooterComponent} from "../footer/footer.component";
 
 @Component({
   selector: 'app-result',
+  standalone: true,
+  imports: [
+    FooterComponent,
+    HeaderComponent
+  ],
   templateUrl: './result.component.html',
   styleUrl: './result.component.css'
 })
