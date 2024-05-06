2e05f49297297b8b119083455beb15e993ef7d43
Siferga <sifergadeveloper@gmail.com>
2024-05-06 12:25:25 +0200

    header update

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
+  standalone: true,
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
